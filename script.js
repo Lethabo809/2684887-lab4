179d0e0c17b18f02da1ba59afc811868528755eb
Lethabo809 <lethabosekgobelaa@gmail.com>
2026-03-02 16:21:47 +0200

    last last

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ async function searchCountry(countryName) {
     try {
         spinner.classList.remove("hidden");
         clearUI();
-        const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+        const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}fullText=true`);
         if (!response.ok) throw new Error("Country not found");
         const data = await response.json();
         const country = data[0];
