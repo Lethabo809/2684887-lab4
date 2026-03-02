26dedc5950361da95bd8d98f60d3b126feb589fd
Lethabo809 <lethabosekgobelaa@gmail.com>
2026-03-02 15:53:37 +0200

    script.js changed

--- script.js
+++ script.js
@@ -27,13 +27,13 @@ async function searchCountry(countryName) {
         const country = data[0];
         countryInfo.innerHTML = `
             <h2>${country.name.common}</h2>
-            <img src="${country.flags.png}" alt="Flag of ${country.name.common}" width="150">
+            <img src="${country.flags.svg || country.flags.png}" alt="Flag of ${country.name.common}" width="150">
             <p><strong>Capital:</strong> ${country.capital?.[0] || "N/A"}</p>
             <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
             <p><strong>Region:</strong> ${country.region}</p>
         `;
-        if (country.borders) {
-            fetchBorders(country.borders);
+        if (country.borders && country.borders.length > 0) {
+            await fetchBorders(country.borders);
         }
     } catch (error) {
         errorMessage.textContent = error.message;
@@ -43,6 +43,7 @@ async function searchCountry(countryName) {
 }
 
 async function fetchBorders(borderCodes) {
+    borderSection.innerHTML = "";
     try {
         const response = await fetch(`https://restcountries.com/v3.1/alpha?codes=${borderCodes.join(",")}`);
         if (!response.ok) throw new Error("Error fetching bordering countries");
@@ -51,7 +52,7 @@ async function fetchBorders(borderCodes) {
             const div = document.createElement("div");
             div.classList.add("border-card");
             div.innerHTML = `
-                <img src="${neighbor.flags.png}" alt="Flag of ${neighbor.name.common}" width="50">
+                <img src="${neighbor.flags.svg || neighbor.flags.png}" alt="Flag of ${neighbor.name.common}" width="50">
                 <p>${neighbor.name.common}</p>
             `;
             borderSection.appendChild(div);
